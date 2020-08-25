5ed219987fbe2225d91039ed34f4634e86f90e90
KJW9458 <zozowlsdn@naver.com>
2020-08-25 15:32:03 +0900

    test

--- search.php
+++ search.php
@@ -4,7 +4,7 @@ $text=$_REQUEST['text'];
   $client_id = "xvRY8SbDOk_9c2eNnV6m";
   $client_secret = "h4Om9RyMvN";
   $encText = urlencode($text);
-  $url = "https://openapi.naver.com/v1/search/webkr.xml?query=".$encText; // json 결과
+  $url = "https://openapi.naver.com/v1/search/webkr.json?query=".$encText; // json 결과
 //  $url = "https://openapi.naver.com/v1/search/blog.xml?query=".$encText; // xml 결과
   $is_post = false;
   $ch = curl_init();
